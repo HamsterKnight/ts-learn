d0f031f696cfd54055cd391fb73fbe5f20a23efc
HamsterKnight <1058268508@qq.com>
2021-04-02 23:41:42 +0800

    feat: ts的基本类型、其他类型、类型别名、函数相关约束类型

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,22 @@ let say = 'abc'
 function add(a:number, b:number) {
   return a + b
 }
+
+function printMenu(): void {
+  console.log('1.登录')
+}
+
+function throwError(msg: string): never {
+  throw new Error(msg)
+}
+
+function alwaysDo(msg: string): never {
+  while(true) {
+    console.log(1230)
+  }
+}
+
+type User = {
+  name: String
+  age: Number
+}
