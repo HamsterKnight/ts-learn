5a8f67910ef10793b1d7ca3f07bbc23aa4db8cdd
HamsterKnight <1058268508@qq.com>
2021-04-06 22:48:54 +0800

    fix: 扑克牌练习和枚举类型了解

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+require('./practice')
 let say = 'abc'
 function add(a:number, b:number) {
   return a + b
--- src/practice.ts
+++ src/practice.ts
@@ -1,3 +1,55 @@
 // 扑克牌练习
 
 // 1. 目标：创建一副扑克牌（不包含大小王）， 打印该扑克牌
+
+type Card = {
+    color: "♠" | "♥" | "♣" | "♦"
+    num: string | number
+}
+
+type Deck = Card[]
+
+function createDeck() {
+    var deck:Deck = []
+    var temp:string|number
+    for(var i = 1; i <= 13; i++) {
+        if(i == 11) {
+            temp = 'j'
+        } else if (i == 12) {
+            temp = 'q'
+        } else if (i == 13) {
+            temp = 'k'
+        } else {
+            temp = i
+        }
+        deck.push({
+            color: "♠",
+            num: temp
+        })
+        deck.push({
+            color: "♥",
+            num: temp
+        })
+        deck.push({
+            color: "♣",
+            num: temp
+        })
+        deck.push({
+            color: "♦",
+            num: temp
+        })
+    }
+
+    return deck
+}
+
+function printDeck(deck:Deck) {
+    var str:string = ''
+    deck.forEach((_, i) => {
+        str+=_.color + _.num + ((i+1) % 4 == 0 ? '\n' : ' ')
+    })
+    console.log(str)
+}
+var deck = createDeck()
+
+printDeck(deck)
